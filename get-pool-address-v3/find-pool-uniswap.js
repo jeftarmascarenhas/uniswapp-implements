c7b4b24100574eefd1c63db8d9e6cc530c1975ca
jeff mascarenhas <jeftarmascarenhas@hotmail.com>
2022-10-05 14:51:42 -0300

    chore: env

---
+++ get-pool-address-v3/find-pool-uniswap.js
@@ -0,0 +1,2 @@
+const { ethers } = require("ethers");
+const {} = require("@uniswap/v3-core/artifacts/contracts/UniswapV3Factory.sol/UniswapV3Factory.json");
--- swap-tokens-v3/uniswap-trader.js
+++ swap-tokens-v3/uniswap-trader.js
@@ -14,3 +14,9 @@ const { getPoolImmutable, getPoolState } = require("./helpers");
 const ERC20ABI = require("./abi.json");
 
 const ALCHEMY_URL_TESTNET = process.env.ALCHEMY_URL_TESTNET;
+const WALLET_ADDRESS = process.env.WALLET_ADDRESS;
+const WALLET_SECRET = process.env.WALLET_SECRET;
+
+const provider = new ethers.providers.JsonRpcProvider(ALCHEMY_URL_TESTNET); // Goerl
+const poolAddress = ""; // UNI/WETH
+const swapRouterAddress = "0xE592427A0AEce92De3Edee1F18E0157C05861564"; // UNI/WETH
